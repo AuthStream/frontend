10ffa4fa195f83514fcaa25ed76630597d42e890
bmchien1 <chienbui10052003@gmail.com>
2025-02-25 23:06:34 +0700

    ui: Dashboard chart

--- src/components/quickAccess.tsx
+++ src/components/quickAccess.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const QuickAccess = () => {
   return (
-    <div className="w-full bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
+    <div className="w-full h-full bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md flex flex-col">
       <h2 className="text-lg font-bold mb-4 text-black dark:text-white">
         Quick Actions
       </h2>
-      <ul className="space-y-2">
+      <ul className="space-y-2 flex-grow">
         <li>
           <Link
             to="/application"
